cc91d4d4cfb1d8bf7e965607eee3cbeb2e103aee
Keerthana Raghuraman <raghukeerthana02@gmail.com>
2026-07-29 00:00:35 +0530

    feat: starting chunking, faiss

--- chunking.py
+++ chunking.py
@@ -1,31 +1,37 @@
-def chunk_text(
-        text:str, 
-        chunk_size:int, 
-        overlap:int
-        
-    )->list[str]:
-    chunks=[]
-    if overlap>=chunk_size:
-        raise ValueError("overlap must be smaller than chunk_size")
-    if chunk_size<=0:
-        raise ValueError("chunk size must be greater than 0")
-    if not text:
-        raise ValueError("length of text must be greater than 0")
-    if overlap<0:
-        raise ValueError("overlap must be greater than or equal to 0")
-    for start in range(0, len(text), chunk_size-overlap):
-        chunks.append(text[start:start+chunk_size])
+import sys
+import faiss
+import numpy as np
 
-    if len(chunks)>1 and len(chunks[-1])<0.5*chunk_size:
-        last=chunks.pop()
-        chunks[-1]+=last[overlap:] if overlap>0 else last
+sys.stdout.reconfigure(encoding='utf-8')
+from sentence_transformers import SentenceTransformer
+model=SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
+sentences=[
+    "Python is a programming language.",
 
-    return chunks
-    
+    "Python is used for machine learning.",
 
-text='heymynameiskeerthanaui'
-chunk_size=5
-overlap=3
-chunks=chunk_text(text, chunk_size,overlap)
-print(chunks)
-print(f"chunks size is {len(chunks)}")
+    "I enjoy eating pizza.",
+
+    "Dogs are loyal animals.",
+
+    "Cats are playful pets.",
+
+    "FastAPI is a Python web framework.",
+
+    "The Eiffel Tower is in Paris."
+]
+chunks=[
+    'Kittens are so cute.',
+    'I love bottles.',
+    'I hate fuzzy sweaters.'
+]
+embeddings=model.encode(chunks)
+print(embeddings.shape)
+query='Tell me about a fuzzy animal'
+embedquery=model.encode(query)
+index=faiss.IndexFlatL2(384)
+index.add(embeddings)
+embedquery=embedquery.reshape(1,-1)
+distances, indices=index.search(embedquery,2)
+print(distances)
+print(indices)
